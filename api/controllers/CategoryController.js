612aa083c1fc59cecf9e82e166c6d66f1f5df323
Satjeetkaur <30588737+Satjeetkaur@users.noreply.github.com>
2017-08-27 21:29:54 -0700

    Update CategoryController.js

--- api/controllers/CategoryController.js
+++ api/controllers/CategoryController.js
@@ -12,58 +12,43 @@ module.exports = {
 		res.view();
 	},
 
-
 	create :function(req, res, next)
 	{
 		Category.create(req.params.all(), function userCreated(err, user){
 			if(err) return next(err);
-			//res.json(user);
-
 			res.redirect('/Category/show/?cid='+ user.cid);
 		});
 	},
 
-	'show': function(req, res, next){
+	'Show_Record': function(req, res, next){
 		Category.find(function foundUser(err, users){
 			if (err) return next(err);
 			if (!users) return next();
-
-			//res.json(user);
-
-			res.view({ 
+  		res.view({ 
 				users: users
-			});
-			
-			 //sails.log('Wow, there are %d users named Finn.  Check it out:', users.length, users);
- 
+	    });
 		});
 	},
 
-	'viewrec': function(req, res, next){
+	'View_Record': function(req, res, next){
 		Category.findOne(req.param('id'), function foundUser(err, user){
 			if (err) return next(err);
 			if (!user) return next();
-
-			//res.json(user);
-
 			res.view({ 
 				user: user
 			});
-			
-			// sails.log('Wow, there are %d users named Finn.  Check it out:', user.length, user);
- 
 		});
 	},
 
-	'deleterec': function(req, res, next){
-
+	'Delete_Record': function(req, res, next){
 		Category.destroy(req.param('cid')).exec(function (err){
   				if (err) {
     				return res.negotiate(err);
   					}
-  						sails.log('record have now been deleted, succesfully');
+  					sails.log('record have now been deleted, succesfully');
  				    return res.ok();
-		});
-	}
-};
+		  });
+	  }
+  };
 
+ 
